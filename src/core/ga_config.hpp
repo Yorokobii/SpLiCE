067cbb45db0902e8573633a8fed0c891ac9dfc85
Dennis Wilson <dennis@d9w.xyz>
2018-06-30 03:24:50 +0200

    ga config doesnt compile

--- src/core/ga_config.hpp
+++ src/core/ga_config.hpp
@@ -1,16 +1,20 @@
-#pragma once
+#ifndef LALACONFIG_HPP
+#define LALACONFIG_HPP
 #include <mecacell/mecacell.h>
 #include <metaconfig/metaconfig.hpp>
 #include "cell.hpp"
 #include "controller.hpp"
 #include "scenario.hpp"
 
-struct BaseController;
+// For jean, errors when using this config:
+// ‘struct Config::GAConfig’ has no member named ‘from_json’
+// template <typename T> void from_json(const nlohmann::json& j, T& e) { e.from_json(j); }
+
 template <typename Cell, typename Conf> class Scenario;
 struct Config {
 	// ---------   STATIC CONFIG   ------------
 	using CtrlType = BaseController;
-	using CellType = Cell<BaseController>;
+	using CellType = Cell<BaseController, Config>;
 	using scenario_t = Scenario<CellType, Config>;
 
 	// ---------   DYNAMICAL CONFIG  ----------
@@ -40,7 +44,12 @@ struct Config {
 	// -- Simulation
 	DECLARE_CONFIG(Config, (int, seed), (double, sim_dt), (double, sim_duration),
 	               (size_t, t_contract), (double, cell_radius), (double, cell_stiffness),
-	               (double, cell_adhesion), (double, fluid_density), (GAConfig, ga))
+	               (double, cell_adhesion), (double, div_radius), (double, fluid_density),
+                 (double, energy_initial), (double, energy_duplicate),
+                 (double, energy_rotate), (double, energy_apoptosis),
+                 (double, energy_contraction), (double, energy_quiescence),
+                 // (int, min_action_age))
+                 (int, min_action_age), (GAConfig, ga))
 
 	Config() {
 		seed = 0;
@@ -51,5 +60,15 @@ struct Config {
 		cell_stiffness = 10.0;
 		cell_adhesion = 5.0;
 		fluid_density = 1e-4;
+    div_radius = 20;
+    energy_initial = 10.0;
+    energy_duplicate = 1.0;
+    energy_rotate = 0.1;
+    energy_apoptosis = 0.5;
+    energy_contraction = 0.1;
+    energy_quiescence = 0.01;
+    min_action_age = 20;
 	}
 };
+
+#endif
