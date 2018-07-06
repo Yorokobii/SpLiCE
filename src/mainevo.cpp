d9abf0c9f10c97509475a8d1ec216200580c3df4
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-07-06 15:20:23 +0200

    mainevo fixes

--- src/mainevo.cpp
+++ src/mainevo.cpp
@@ -7,70 +7,6 @@
 // #include "core/evaluators.hpp"
 #include "core/capture.hpp"
 
-int main(int argc, char** argv) {
-  Config cfg(argc, argv);
-
-  GAGA::GA<Config::CtrlType> ga(argc, argv);
-
-  ga.setEvaluator([cfg](auto& individual, int) {
-        Config::scenario_t scenario(cfg);
-        scenario.controller = Config::CtrlType(individual.dna);
-        scenario.init();
-        while (!scenario.finished()) scenario.loop();
-
-        std::vector<std::vector<double>> footprints;
-        std::vector<double> fp;
-
-        //compute nconn footprint
-        float nconn;
-        for(auto& c : scenario.getWorld().cells)
-          for(auto& conn : c->getBody().cellConnections)
-            nconn += (conn->adhCoef > 0.0) ? 1.0 : 0.0;
-        nconn /= scenario.getWorld().cells.size();
-        fp.push_back(nconn);
-        fp.push_back(scenario.getWorld().cells.size()/10.0);
-
-        // for(auto& v : cme.getFootprint(scenario))
-        //   fp.push_back(v);
-        footprints.push_back(getFootprint(scenario));
-
-        footprints.push_back(fp);
-        individual.footprint = footprints;
-
-        individual.fitnesses["DistanceEnergy"] = scenario.fit;
-        // individual.fitnesses["ShapeEnergy"] = scenario.shapefit;
-      },
-      "DistanceEnergy");
-
-  ga.setPopSize(cfg.populationSize);
-  ga.setMutationProba(cfg.mutationRate);
-  ga.setCrossoverProba(cfg.crossoverRate);
-  ga.setVerbosity(cfg.verbosity);
-  ga.setNbThreads(cfg.nbThreads);
-  ga.setSaveFolder("evos");
-  ga.enableNovelty();
-
-  // ga.setSaveParetoFront(true);
-
-  if (cfg.speciation) {
-    ga.enableSpeciation();
-    ga.setMinSpecieSize(cfg.minSpecieSize);
-    ga.setSpeciationThreshold(cfg.speciationThreshold_base);
-    ga.setSpeciationThresholdIncrement(cfg.speciationThreshold_increment);
-    ga.setMaxSpeciationThreshold(cfg.speciationThreshold_max);
-    ga.setMinSpeciationThreshold(cfg.speciationThreshold_min);
-    ga.setIndDistanceFunction([](const auto& a, const auto& b) {
-      return Config::CtrlType::getDistance(a.dna, b.dna);
-    });
-  }
-
-	ga.initPopulation([]() {
-                      return Config::CtrlType::random();
-                    });
-  ga.step(cfg.nbGenerations);
-  return 0;
-}
-
 
 template <typename Cell> double computeSphericity(const unordered_set<Cell *> clust, float radius) {
   MecaCell::Grid<Cell *> grid(radius / 3.0);
@@ -154,4 +90,71 @@ template <typename Cell> MecaCell::Vec getLongestDistance(std::vector<Cell *> ce
     }
   }
   return longest;
+}
+
+
+
+
+int main(int argc, char** argv) {
+  Config cfg(argc, argv);
+
+  GAGA::GA<Config::CtrlType> ga(argc, argv);
+
+  ga.setEvaluator([cfg](auto& individual, int) {
+        Config::scenario_t scenario(cfg);
+        scenario.controller = Config::CtrlType(individual.dna);
+        scenario.init();
+        while (!scenario.finished()) scenario.loop();
+
+        std::vector<std::vector<double>> footprints;
+        std::vector<double> fp;
+
+        //compute nconn footprint
+        float nconn;
+        for(auto& c : scenario.getWorld().cells)
+          for(auto& conn : c->getBody().cellConnections)
+            nconn += (conn->adhCoef > 0.0) ? 1.0 : 0.0;
+        nconn /= scenario.getWorld().cells.size();
+        fp.push_back(nconn);
+        fp.push_back(scenario.getWorld().cells.size()/10.0);
+
+        // for(auto& v : cme.getFootprint(scenario))
+        //   fp.push_back(v);
+        footprints.push_back(getFootprint(scenario));
+
+        footprints.push_back(fp);
+        individual.footprint = footprints;
+
+        individual.fitnesses["DistanceEnergy"] = scenario.fit;
+        // individual.fitnesses["ShapeEnergy"] = scenario.shapefit;
+      },
+      "DistanceEnergy");
+
+  ga.setPopSize(cfg.populationSize);
+  ga.setMutationProba(cfg.mutationRate);
+  ga.setCrossoverProba(cfg.crossoverRate);
+  ga.setVerbosity(cfg.verbosity);
+  ga.setNbThreads(cfg.nbThreads);
+  ga.setSaveFolder("evos");
+  ga.enableNovelty();
+
+  // ga.setSaveParetoFront(true);
+
+  if (cfg.speciation) {
+    ga.enableSpeciation();
+    ga.setMinSpecieSize(cfg.minSpecieSize);
+    ga.setSpeciationThreshold(cfg.speciationThreshold_base);
+    ga.setSpeciationThresholdIncrement(cfg.speciationThreshold_increment);
+    ga.setMaxSpeciationThreshold(cfg.speciationThreshold_max);
+    ga.setMinSpeciationThreshold(cfg.speciationThreshold_min);
+    ga.setIndDistanceFunction([](const auto& a, const auto& b) {
+      return Config::CtrlType::getDistance(a.dna, b.dna);
+    });
+  }
+
+	ga.initPopulation([]() {
+                      return Config::CtrlType::random();
+                    });
+  ga.step(cfg.nbGenerations);
+  return 0;
 }
