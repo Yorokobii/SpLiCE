72206fd34ee7f5672fa3f846f5f8948710deaf36
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-07-06 15:33:14 +0200

    copied the code for the complex morphology into the main code

--- src/mainevo.cpp
+++ src/mainevo.cpp
@@ -7,94 +7,6 @@
 // #include "core/evaluators.hpp"
 #include "core/capture.hpp"
 
-
-template <typename Cell> double computeSphericity(const unordered_set<Cell *> clust, float radius) {
-  MecaCell::Grid<Cell *> grid(radius / 3.0);
-  for (auto &c : clust) grid.insert(c);
-  auto s = grid.computeSphericity();
-  return s;
-}
-
-std::vector<double> getFootprint(Config::scenario_t &sc) {
-  if (sc.getWorld().cells.size() == 0) {
-    return {{0, 0}};
-  }
-  auto clusters =
-      ClusterTools::getClusters(sc.getWorld().cells);
-  size_t biggestClusterId = 0;
-  size_t biggestClusterSize = clusters[biggestClusterId].size();
-  for (size_t i = 1; i < clusters.size(); ++i) {
-    if (clusters[i].size() > biggestClusterSize) {
-      biggestClusterSize = clusters[i].size();
-      biggestClusterId = i;
-    }
-  }
-  return {{static_cast<double>(biggestClusterSize), 0.0}};
-
-  if (sc.getWorld().cells.size() == 0) {
-    return {{-1, -1, -1, -1}};
-  }
-
-  auto sphericity = computeSphericity(clusters[biggestClusterId], sc.config.originalRadius);
-  vector<typename Config::scenario_t::cell_t *> biggestClusterVec;
-  biggestClusterVec.reserve(clusters[biggestClusterId].size());
-  MecaCell::Vec centroid(0, 0, 0);
-  for (auto &c : clusters[biggestClusterId]) {
-    biggestClusterVec.push_back(c);
-    centroid += c->getPosition();
-  }
-  if (biggestClusterSize > 0) {
-    centroid /= static_cast<double>(biggestClusterSize);
-  }
-
-  auto longestAxis = getLongestDistance(biggestClusterVec);
-  double longestProj2 = 0;
-  double longestProj3 = 0;
-  double longestDist = longestAxis.length();
-  if (longestDist > 0) {
-    MecaCell::Vec axis2 = longestAxis.ortho().normalized();
-    MecaCell::Vec axis3 = longestAxis.cross(axis2).normalized();
-    for (size_t i = 0; i < biggestClusterSize; ++i) {
-      for (size_t j = i + 1; j < biggestClusterSize; ++j) {
-        auto AB =
-            biggestClusterVec[i]->getPosition() - biggestClusterVec[j]->getPosition();
-        auto proj2 = fabs(AB.dot(axis2));
-        auto proj3 = fabs(AB.dot(axis3));
-        if (longestProj2 < proj2) longestProj2 = proj2;
-        if (longestProj3 < proj3) longestProj3 = proj3;
-      }
-    }
-  }
-  double normalizedNbCells = ClusterTools::sqAsympt(
-      biggestClusterSize, 0.002);  // counts less after 1000 cells.
-  double ratioProj2 = 1.0;
-  double ratioProj3 = 1.0;
-  if (longestDist > 0) {
-    ratioProj2 = longestProj2 / longestDist;
-    ratioProj3 = longestProj3 / longestDist;
-  }
-  return {{normalizedNbCells, sphericity, ratioProj2, ratioProj3}};
-}
-
-template <typename Cell> MecaCell::Vec getLongestDistance(std::vector<Cell *> cells) {
-  MecaCell::Vec longest(0, 0, 0);
-  double sql = 0;
-  for (size_t i = 0; i < cells.size(); ++i) {
-    for (size_t j = 0; j < cells.size(); ++j) {
-      auto axis = cells[i]->getPosition() - cells[j]->getPosition();
-      auto l = axis.sqlength();
-      if (l > sql) {
-        sql = l;
-        longest = axis;
-      }
-    }
-  }
-  return longest;
-}
-
-
-
-
 int main(int argc, char** argv) {
   Config cfg(argc, argv);
 
@@ -120,7 +32,84 @@ int main(int argc, char** argv) {
 
         // for(auto& v : cme.getFootprint(scenario))
         //   fp.push_back(v);
-        footprints.push_back(getFootprint(scenario));
+
+        if (scenario.getWorld().cells.size() == 0) {
+          return {{0, 0}};
+        }
+        auto clusters =
+            ClusterTools::getClusters(scenario.getWorld().cells);
+        size_t biggestClusterId = 0;
+        size_t biggestClusterSize = clusters[biggestClusterId].size();
+        for (size_t i = 1; i < clusters.size(); ++i) {
+          if (clusters[i].size() > biggestClusterSize) {
+            biggestClusterSize = clusters[i].size();
+            biggestClusterId = i;
+          }
+        }
+        return {{static_cast<double>(biggestClusterSize), 0.0}};
+
+        if (scenario.getWorld().cells.size() == 0) {
+          return {{-1, -1, -1, -1}};
+        }
+
+        MecaCell::Grid<Config::CellType *> grid(cfg.originalRadius / 3.0);
+        for (auto &c : clust) grid.insert(c);
+        auto sphericity = grid.computeSphericity();
+
+
+
+        vector<typename Config::CellType *> biggestClusterVec;
+        biggestClusterVec.reserve(clusters[biggestClusterId].size());
+        MecaCell::Vec centroid(0, 0, 0);
+        for (auto &c : clusters[biggestClusterId]) {
+          biggestClusterVec.push_back(c);
+          centroid += c->getPosition();
+        }
+        if (biggestClusterSize > 0) {
+          centroid /= static_cast<double>(biggestClusterSize);
+        }
+
+        MecaCell::Vec longestAxis(0, 0, 0);
+        double sql = 0;
+        for (size_t i = 0; i < biggestClusterVec.size(); ++i) {
+          for (size_t j = 0; j < biggestClusterVec.size(); ++j) {
+            auto axis = biggestClusterVec[i]->getPosition() - biggestClusterVec[j]->getPosition();
+            auto l = axis.sqlength();
+            if (l > sql) {
+              sql = l;
+              longestAxis = axis;
+            }
+          }
+        }
+
+        double longestProj2 = 0;
+        double longestProj3 = 0;
+        double longestDist = longestAxis.length();
+        if (longestDist > 0) {
+          MecaCell::Vec axis2 = longestAxis.ortho().normalized();
+          MecaCell::Vec axis3 = longestAxis.cross(axis2).normalized();
+          for (size_t i = 0; i < biggestClusterSize; ++i) {
+            for (size_t j = i + 1; j < biggestClusterSize; ++j) {
+              auto AB =
+                  biggestClusterVec[i]->getPosition() - biggestClusterVec[j]->getPosition();
+              auto proj2 = fabs(AB.dot(axis2));
+              auto proj3 = fabs(AB.dot(axis3));
+              if (longestProj2 < proj2) longestProj2 = proj2;
+              if (longestProj3 < proj3) longestProj3 = proj3;
+            }
+          }
+        }
+        double normalizedNbCells = ClusterTools::sqAsympt(
+            biggestClusterSize, 0.002);  // counts less after 1000 cells.
+        double ratioProj2 = 1.0;
+        double ratioProj3 = 1.0;
+        if (longestDist > 0) {
+          ratioProj2 = longestProj2 / longestDist;
+          ratioProj3 = longestProj3 / longestDist;
+        }
+
+
+        footprints.push_back({normalizedNbCells, sphericity, ratioProj2, ratioProj3});
 
         footprints.push_back(fp);
         individual.footprint = footprints;
