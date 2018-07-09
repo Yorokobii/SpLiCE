d058281eccff1a91f69ff54b41ecd3f84ea98736
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-07-09 12:01:17 +0200

    added distance fitness

--- src/core/scenario.hpp
+++ src/core/scenario.hpp
@@ -154,7 +154,14 @@ template <typename cell_t, typename ctrl_t, typename cfg_t> class Scenario {
     // }
 
     //compute com fitness
-    fit = 0.0;
+    //compute biggest cell2cell distance
+    float distc2c = 0.0;
+    for(auto& c1 : world.cells)
+      for(auto& c2 : world.cells)
+        if((c1->getPosition() - c2->getPosition()).length() > distc2c)
+          distc2c = (c1->getPosition() - c2->getPosition()).length();
+
+    fit = (MecaCell::Vec::zero() - com).length() / distc2c;
 
     int connections_per_cell = 0;
     for (auto& c : world.cells) {
