50eac85e5f395aace3b8588e263162f6ed14221e
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-07-09 14:46:08 +0200

    fix duplication -> duplicate

--- src/core/scenario.hpp
+++ src/core/scenario.hpp
@@ -166,7 +166,7 @@ template <typename cell_t, typename ctrl_t, typename cfg_t> class Scenario {
     for(auto& c : world.cells){
       if(c->nconn > 4){
         c->getBody().setStiffness(999999);
-        c->action_outputs = {"quiescence", "rotation", "duplicate"};
+        c->action_outputs = {"quiescence", "rotate", "duplicate"};
       }
     }
 
