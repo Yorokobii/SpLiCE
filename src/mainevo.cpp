14adca4e161c122d48710afd4044d718e6159591
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-07-12 16:13:25 +0200

    DBG - novelty with default dist func

--- src/mainevo.cpp
+++ src/mainevo.cpp
@@ -54,7 +54,7 @@ int main(int argc, char** argv) {
   ga.setVerbosity(cfg.verbosity);
   ga.setNbThreads(cfg.nbThreads);
   ga.setSaveFolder("evos");
-  // ga.enableNovelty();
+  ga.enableNovelty();
   // ga.setComputeFootprintDistanceFunction([](const auto& f0, const auto& f1) {
   //   assert(f0.size() == f1.size());
 	// 	double d = 0;
