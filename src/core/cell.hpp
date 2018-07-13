bbeb1d3d55a64c75335a8805bcff4f08c76b0bd5
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-07-13 13:15:57 +0200

    increased the connection limit to which a cell cannot duplicate anymore from 5 to 7

--- src/core/cell.hpp
+++ src/core/cell.hpp
@@ -92,7 +92,7 @@ template <typename Controller, typename Config> class Cell
     std::string action = action_outputs[actionMax];
 
     if (action == "duplicate") {
-      if (energy >= config.energyDuplicate && (config.maxCells != 0 ? w.cells.size() < config.maxCells : true) && nconn < 5) {
+      if (energy >= config.energyDuplicate && (config.maxCells != 0 ? w.cells.size() < config.maxCells : true) && nconn < 7) {
         // cell duplicate
         Vec dpos {sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta)};
         Vec child_pos = dpos * config.divRadius + this->getPosition();
