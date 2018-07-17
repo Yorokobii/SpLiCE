2584939d3a13e6e62f6b00ca7a36bbbe78d8ed73
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-07-17 12:17:45 +0200

    put back totalCom

--- src/core/scenario.hpp
+++ src/core/scenario.hpp
@@ -24,7 +24,7 @@ template <typename cell_t, typename ctrl_t, typename cfg_t> class Scenario {
   ctrl_t controller;
   MecaCell::Vec com = MecaCell::Vec::zero();
   MecaCell::Vec prevCom = MecaCell::Vec::zero();
-  // MecaCell::Vec totalCom = MecaCell::Vec::zero();
+  MecaCell::Vec totalCom = MecaCell::Vec::zero();
 
  protected:
   double currentTime = 0;
@@ -186,16 +186,16 @@ template <typename cell_t, typename ctrl_t, typename cfg_t> class Scenario {
     //       if((c1->getPosition() - c2->getPosition()).length() > distc2c)
     //         distc2c = (c1->getPosition() - c2->getPosition()).length();
 
-    // totalCom += (com - prevCom);
+    totalCom += (com - prevCom);
 
-    // fit = totalCom.length();
-    fit = (MecaCell::Vec::zero() - com).length();
+    fit = totalCom.length();
+    // fit = (MecaCell::Vec::zero() - com).length();
 
-    // prevCom = com;
+    prevCom = com;
 
     MecaCell::logger<MecaCell::DBG>(":S| ", currentTime, " ", worldAge, " ", energy, " ",
                                     world.cells.size(), " ", gcomm, " ",
-                                    /*totalCom*/com, " ", fit, " ");
+                                    totalCom/*com*/, " ", fit, " ");
 
   }
 
