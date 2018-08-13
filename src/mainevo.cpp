374284cacdabe547e661564d6e73d089821d557e
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-08-13 12:20:39 +0200

    distance fitness normalized to 0..1 - added average force novelty

--- src/mainevo.cpp
+++ src/mainevo.cpp
@@ -25,22 +25,26 @@ int main(int argc, char** argv) {
         float nconn = 0.0;
         float ncontr = 0.0;
         float next = 0.0;
+        float avgForce = 0.0;
         int nCellsContracted = 0.0;
         for(auto& c : scenario.getWorld().cells){
           for(auto& conn : c->getBody().cellConnections)
             nconn += (conn->adhCoef > 0.0) ? 1.0 : 0.0;
           ncontr += c->contractionCount;
           next += c->extensionCount;
+          avgForce += c->contractForce;
           if(c->contractionCount) nCellsContracted++;
         }
         nconn /= scenario.getWorld().cells.size();
         ncontr /= scenario.getWorld().cells.size();
         next /= scenario.getWorld().cells.size();
+        avgForce /= scenario.getWorld().cells.size();
         // footprints[0].push_back(1.0 - 1.0/max((double)nconn, 1.0));
-        footprints[0].push_back(1.0 - 1.0/max((double)ncontr/100, 1.0));
+        // footprints[0].push_back(1.0 - 1.0/max((double)ncontr/100, 1.0));
         // footprints[0].push_back(1.0 - 1.0/max((double)next/100, 1.0));
         // footprints[0].push_back(1.0 - 1.0/max((double)nCellsContracted, 1.0));
         // footprints[0].push_back(1.0 - 1.0/(double)scenario.getWorld().cells.size());
+        footprints[0].push_back(avgForce);
 
         // if (scenario.getWorld().cells.size() != 0) {
         //   auto clusters =
@@ -50,8 +54,8 @@ int main(int argc, char** argv) {
 
         individual.footprint = footprints;
 
-        // individual.fitnesses["DistanceEnergy"] = 1.0 - 1.0/max(scenario.fit, 1.0);
-        individual.fitnesses["DistanceEnergy"] = scenario.fit;
+        individual.fitnesses["DistanceEnergy"] = 1.0 - 1.0/max(scenario.fit/100, 1.0);
+        // individual.fitnesses["DistanceEnergy"] = scenario.fit;
       },
       "DistanceEnergy");
 
@@ -61,18 +65,18 @@ int main(int argc, char** argv) {
   ga.setVerbosity(cfg.verbosity);
   ga.setNbThreads(cfg.nbThreads);
   ga.setSaveFolder("evos");
-  // ga.enableNovelty();
-  // ga.setKNN(cfg.populationSize);
-  // ga.setComputeFootprintDistanceFunction([](const auto& f0, const auto& f1) {
-  //   assert(f0.size() == f1.size());
-	// 	double d = 0;
-	// 	for (size_t i = 0; i < f0.size(); ++i) {
-	// 		for (size_t j = 0; j < f0[i].size(); ++j) {
-	// 			d += std::pow(f0[i][j] - f1[i][j], 2);
-	// 		}
-	// 	}
-	// 	return sqrt(d);
-  // });
+  ga.enableNovelty();
+  ga.setKNN(cfg.populationSize);
+  ga.setComputeFootprintDistanceFunction([](const auto& f0, const auto& f1) {
+    assert(f0.size() == f1.size());
+		double d = 0;
+		for (size_t i = 0; i < f0.size(); ++i) {
+			for (size_t j = 0; j < f0[i].size(); ++j) {
+				d += std::pow(f0[i][j] - f1[i][j], 2);
+			}
+		}
+		return sqrt(d);
+  });
 
   // ga.setSaveParetoFront(true);
 
