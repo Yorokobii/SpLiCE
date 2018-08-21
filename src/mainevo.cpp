a32721e50cb51228976adde1ba17e4e8775ec5dc
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-08-21 14:57:58 +0200

    put back novelties

--- src/mainevo.cpp
+++ src/mainevo.cpp
@@ -55,8 +55,8 @@ int main(int argc, char** argv) {
         individual.footprint = footprints;
 
         // individual.fitnesses["DistanceEnergy"] = 1.0 - exp(-scenario.fit);
-        // individual.fitnesses["DistanceEnergy"] = max(0.0, 1.0 - 1.0/scenario.fit);
-        individual.fitnesses["DistanceEnergy"] = scenario.fit;
+        individual.fitnesses["DistanceEnergy"] = max(0.0, 1.0 - 1.0/scenario.fit);
+        // individual.fitnesses["DistanceEnergy"] = scenario.fit;
       },
       "DistanceEnergy");
 
@@ -66,7 +66,7 @@ int main(int argc, char** argv) {
   ga.setVerbosity(cfg.verbosity);
   ga.setNbThreads(cfg.nbThreads);
   ga.setSaveFolder("evos");
-  // ga.enableNovelty();
+  ga.enableNovelty();
   // ga.setKNN(cfg.populationSize);
   ga.setKNN(cfg.populationSize/10);
   ga.setComputeFootprintDistanceFunction([](const auto& f0, const auto& f1) {
