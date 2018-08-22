934ed6de50c3e5ff0d741cf005be237ebda0d4b5
BOEHM GUILLAUME <guillaume.boehm@etu.unistra.fr>
2018-08-22 10:46:51 +0200

    simDuration limit is of 1 for 100 updates - fix

--- src/core/scenario.hpp
+++ src/core/scenario.hpp
@@ -230,7 +230,7 @@ template <typename cell_t, typename ctrl_t, typename cfg_t> class Scenario {
   }
 
   world_t& getWorld() { return world; }
-  bool finished() { return ((duration < 1 ? currentTime > 1000 : currentTime > duration) || energy <= 0.0 ||
+  bool finished() { return ((duration < 0.01 ? currentTime > 10 : currentTime > duration) || energy <= 0.0 ||
                             world.cells.size() == 0); }
 };
 
